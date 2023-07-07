de23c5bec2f46ea9c6f1af7e46cd983776b9d252
archies07 <138809776+archies07@users.noreply.github.com>
2023-07-07 10:51:03 +0530

    Add project files.

---
+++ Blog.web/Data/blogdbcontext.cs
@@ -0,0 +1,15 @@
+﻿using Blog.web.Models.Domin;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.web.Data
+{
+    public class blogdbcontext :DbContext
+    {
+        public blogdbcontext(DbContextOptions options): base(options)
+        {
+
+        }
+        public DbSet<blogpost>blogposts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
---
+++ Blog.web/Models/Domin/Tag.cs
@@ -0,0 +1,11 @@
+﻿namespace Blog.web.Models.Domin
+{
+    public class Tag
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+
+        public ICollection<blogpost> Tags { get; set; }
+    }
+}
